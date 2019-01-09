d88ad39c949bd68319824000bdb4dc467fed6cba
Jeremy Stark <razestudiosdev@gmail.com>
2019-01-09 14:39:50 -0500

    Updated Stark_Day1

--- CW/CW_01/Stark_Day1.m
+++ CW/CW_01/Stark_Day1.m
@@ -1,9 +1,23 @@
 clc
 clear
-disp('Jeremy Mechanical');
+
+disp('Jeremy Stark - Due Jan-29-2019');
+disp('ENGR-297 Class # 22749');
+
+disp('Chapter 12 problems page 106 #5');
+disp('Type in matrixa and answer questions');
+
+value = (3125)^(1/5);
+hmmm = 3125^1/5;
+
+seven = nthroot(343,3);
+num7 = 343^(1/3);
+
+fifteen = exp(2.718);
+inversed = exp(log(25));
 
 argument1 = 3+4;
 argument2 = 4+5;
 argument_a = -2;
 output_a = sqrt(argument_a);
-fprintf('The end');
+fprintf('The end\n');
