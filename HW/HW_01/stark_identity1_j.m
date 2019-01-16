2b902c11812ec109ee489509e7de1efd75488db0
Jeremy Stark <razestudiosdev@gmail.com>
2019-01-16 08:12:05 -0500

    HW_01 Finished

---
+++ HW/HW_01/stark_identity1_j.m
@@ -0,0 +1,32 @@
+clc
+clear
+disp('Name and Date:         Jeremy Stark 01/16/2019');
+disp('Course and Section:    ENGR297 and class # 22749');
+disp('Problem:               Matlab Homework 1');
+disp('Statement:             Using variables to store solutions to math problems');
+disp(' ');
+disp('problem 3a')
+value3a = (3^7 * log10(76))/(7^3 + 546) + nthroot(910, 3);
+disp(value3a);
+disp(' ');
+disp('problem 3b');
+value3b = 43 * (nthroot(250, 4) + 23)^2/(exp(45-3^3));
+disp(value3b);
+disp(' ');
+
+x = 13.5;
+
+disp('problem 5a');
+value5a = x^3 + 5*x^2 - 26.7*x - 52;
+disp(value5a);
+disp(' ');
+
+disp('problem 5b');
+value5b = (sqrt(14*x^3))/(exp(3*x));
+disp(value5b);
+disp(' ');
+
+disp('problem 5c');
+value5c = log10(abs(x^2-x^3));
+disp(value5c);
+disp(' ');
