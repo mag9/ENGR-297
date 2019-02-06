046b9a89e6daf2bda83e1f6ed478ba795253e980
Jeremy Stark <razestudiosdev@gmail.com>
2019-02-06 14:30:59 -0500

    CW_04 Added

---
+++ CW/CW_04/stark_cw_04.m
@@ -0,0 +1,13 @@
+clc
+clear
+disp('Name and Date:         Jeremy Stark 02/06/2019');
+disp('Course and Section:    ENGR297 and class # 22749');
+disp('Problem:               Matlab Classwork 4');
+disp('Statement:             ');
+disp(' ');
+
+% volume = input('What is the volume of the sphere?')
+% disp('Volume is measured in meters cubed')
+% disp(volume)
+
+test = input('enter numbers', 's')
